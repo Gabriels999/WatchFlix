ed84fd25736486c807f2f5158408c7080adf92e7
Gabriel Santos <gab.tec97@gmail.com>
2022-03-11 12:02:28 -0300

    Implement search bar

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ const API_KEY = "api_key=d7ffe740f407eb01bbd10d9090d1740d";
 const BASE_URL = "https://api.themoviedb.org/3";
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
+const searchURL = BASE_URL + "/search/movie?" + API_KEY;
 
 const main = document.querySelector("#main");
+const form = document.querySelector("#form");
+const search = document.querySelector("#search");
 
 function getMovies(url) {
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data.results);
+      console.log(data);
       showMovies(data.results);
     });
 }
@@ -24,14 +27,14 @@ function getColor(vote) {
 function showMovies(data) {
   main.innerHTML = "";
   data.forEach((movie) => {
-    const { title, original_title, poster_path, vote_average, overview } =
+    const { title, poster_path, vote_average, overview } =
       movie;
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
         <img
           src="${IMG_URL + poster_path}"
-          alt="${original_title}"
+          alt="${title}"
         />
         <div class="movie-info">
           <h3>${title}</h3>
@@ -47,3 +50,12 @@ function showMovies(data) {
   });
 }
 getMovies(API_URL);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(searchURL + "&query=" + searchTerm);
+  } else getMovies(API_URL);
+});
